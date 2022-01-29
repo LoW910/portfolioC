7b4bb3cb1d98e2d0bc2f0725fa2876b88de28c1e
Daniel Soots <danielsoots@icloud.com>
2022-01-29 16:32:07 -0500

    created sendmesage (not functional)

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -4,10 +4,25 @@ namespace Portfolio.Controllers
 {
     public class ContactController : Controller
     {
+
         [HttpGet("contact")] // get req for /contact
 
         public ViewResult Index(){
             return View();
         }
+
+        [HttpPost("sendMessage")]
+        public IActionResult SendMessage(string firstName, string lastName, string email, string message) {
+            ViewBag.first = firstName;
+            ViewBag.last = lastName;
+            ViewBag.mail = email;
+            ViewBag.msg = message;
+            return View();
+        }
+
+// [HttpGet("confirm")]
+//         public ViewResult Confirm() {
+//             return View();
+//         }
     }
 }
